b390ec5a31ec7deaf202e70edaa9e360e26523bd
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2025-06-15 12:39:01 -0300

    add: hello world in response handler

--- srcs/handlers/handler.c
+++ srcs/handlers/handler.c
@@ -1,11 +1,29 @@
 #include "http.h"
 
+int allocate_buffer(char **buffer, size_t size) {
+    *buffer = malloc(size);
+    if (*buffer == NULL) {
+        perror("Failed to allocate memory for buffer");
+        return 0;
+    }
+    return -1;
+}
+
 void *handle_client(void *arg) {
     int clientSocketFd = (int)(intptr_t)arg;
-    char *buffer = malloc(BUFFER_SIZE);
+    char *buffer;
+    if (!allocate_buffer(&buffer, BUFFER_SIZE)) {
+        close(clientSocketFd);
+        return NULL;
+    }
     
     recv(clientSocketFd, buffer, BUFFER_SIZE, 0);
-    printf("buffer: %s\n", buffer);
+    const char *response = "HTTP/1.1 200 OK\r\n"
+                          "Content-Type: text/plain\r\n"
+                          "Content-Length: 11\r\n"
+                          "\r\n"
+                          "Hello World!";
+    send(clientSocketFd, response, strlen(response), 0);
     
     close(clientSocketFd);
     free(buffer);
